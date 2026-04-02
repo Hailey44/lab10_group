e98889e195d54f074003f9611137925fda1ef0a3
lydia-ko <lydia.ko@gwu.edu>
2026-04-02 15:31:38 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, lydia!");
         System.out.println("my name is lydia");
     }
 }
